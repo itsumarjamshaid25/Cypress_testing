598e408f1ef0b9ae05c2d1be15978d5c24618f09
itsumarjamshaid25 <umarjamshaid25@gmail.com>
2023-08-09 01:24:40 +0500

    here is begining of code of Cypress

---
+++ cypress/e2e/Vertex_Special_home.cy.js
@@ -0,0 +1,19 @@
+//we can run test case in cypress pplication 
+//2 in terminal
+//3 default browser is electron if you want to change it 
+//4 npx cypress run --browser nmae --headed to open test in specific brower 
+//5 npx cypress run used to run in terminal 
+//6 npx cypress run --headed to open defualt brwser and run 
+//7 its run all the instant changes   
+
+describe('Home page', ()=>{
+    it('navigation check',()=>
+    {
+        cy.visit("https://vertexspecial.com/")
+       
+        //cy.title().should('eq','Vertex Special Technologies') //here thi is used for assertion 
+        cy.get(".bar-line").click()
+        cy.get(".flaticon-facebook").click()
+    })
+
+})
